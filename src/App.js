c91a5c2b9e5cdf6da2cfce3af9b6052e095c223e
Omkar Ghate <otg0231@gmail.com>
2021-08-11 07:39:16 +0530

    chore: added date input field and check button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ export default function App() {
         <h1>Birthday Palindrome</h1>
         <h2>Check if your birthday is a palindrome...</h2>
       </div>
+
+      <div className="main">
+        <input type="date" required></input>
+        <button className="btn" onClick={handleClick}>
+          Check
+        </button>
+      </div>
     </div>
   );
 }
