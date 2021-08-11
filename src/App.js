d5c3d17222277cbd4ab893fec582dfb97922e240
Omkar Ghate <otg0231@gmail.com>
2021-08-11 07:38:42 +0530

    chore: added headings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="header">
+        <h1>Birthday Palindrome</h1>
+        <h2>Check if your birthday is a palindrome...</h2>
+      </div>
     </div>
   );
 }
