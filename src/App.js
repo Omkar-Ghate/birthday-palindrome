7ed8b998a65bf587a51ef2c8b123bf84d7bd4610
Omkar Ghate <otg0231@gmail.com>
2021-08-11 07:50:29 +0530

    chore: setup basic ip/op

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  function handleClick() {
-    console.log("Clicked");
+  var [date, setDate] = useState("");
+  var [message, setMessage] = useState("");
+
+  function handleChange(event) {
+    setDate(event.target.value);
+  }
+
+  function handleClick(date) {
+    if (date === "") {
+      setMessage("Please enter the date.");
+    } else {
+      setMessage("The date entered is " + date);
+    }
   }
 
   return (
@@ -13,10 +25,12 @@ export default function App() {
       </div>
 
       <div className="main">
-        <input type="date" required></input>
-        <button className="btn" onClick={handleClick}>
+        <input type="date" onChange={() => handleChange(event)}></input>
+        <button className="btn" onClick={() => handleClick(date)}>
           Check
         </button>
+
+        <h2>{message}</h2>
       </div>
     </div>
   );
